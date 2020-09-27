032a345585fc4e77f30490cc14c7e417b31f9a79
jaironsousa1 <jaironsousa@gmail.com>
2020-09-27 19:29:11 -0300

    01 - Criação do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaiComponent } from './pai/pai.component';
+import { FilhoComponent } from './filho/filho.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaiComponent,
+    FilhoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/filho/filho.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filho',
+  templateUrl: './filho.component.html',
+  styleUrls: ['./filho.component.css']
+})
+export class FilhoComponent {
+
+
+}
